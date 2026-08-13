10411f8eb2f38bbbf0726e9700465cc48c43a8d4
Qingfeng-ck <3387509943@qq.com>
2026-08-13 12:30:00 +0800

    创建主程序

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("✨ Aurora Launcher 启动成功！");
+        System.out.println("🚀 准备进入 Minecraft...");
+    }
+}
