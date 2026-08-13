379070a8aae2213877cfaff6a51ee6544715dfb7
Qingfeng-ck <3387509943@qq.com>
2026-08-13 12:42:15 +0800

    更新 Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,21 @@ public class Main {
             .uri(URI.create("https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"))
             .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println("获取到版本清单：");
-        System.out.println(response.body());
+
+        String json = response.body();
+        
+        // 提取最新版本的ID和URL（用简单方法）
+        String latestId = extractValue(json, "\"id\":\"", "\"");
+        String latestUrl = extractValue(json, "\"url\":\"", "\"");
+        
+        System.out.println("最新版本：" + latestId);
+        System.out.println("版本JSON地址：" + latestUrl);
+    }
+    
+    // 简单提取字符串
+    public static String extractValue(String text, String start, String end) {
+        int startIndex = text.indexOf(start) + start.length();
+        int endIndex = text.indexOf(end, startIndex);
+        return text.substring(startIndex, endIndex);
     }
 }
